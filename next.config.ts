2632071eec0a034ca5a3f020000ccf7ca3dd21e0
Punnawich02 <punnawich_dachin@cmu.ac.th>
2025-04-10 14:44:41 +0700

    fix: update next.config.ts to use CommonJS module syntax and define redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
+}
