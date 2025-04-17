11407118387a2b4657dacb7ebe70d7cba505bcc4
Punnawich02 <punnawich_dachin@cmu.ac.th>
2025-04-17 14:25:39 +0700

    fix: update link for data service card and refactor card rendering structure

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -17,7 +17,7 @@ import { motion } from "framer-motion";
 const cardData = [
   {
     title: "บริการข้อมูล",
-    link: "data",
+    link: "data_service",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
     icon: <Database className="w-16 h-16" color="#FFF" strokeWidth={1.5} />,
@@ -96,26 +96,25 @@ export default function ServicesPage() {
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.8 }}
                   viewport={{ once: true, amount: 0.1 }}
+                ></motion.div>
+                <Card
+                  key={index}
+                  className="hover:scale-105 transition-transform duration-300 ease-in-out hover:cursor-pointer h-full"
                 >
-                  <Card
-                    key={index}
-                    className="hover:scale-105 transition-transform duration-300 ease-in-out hover:cursor-pointer h-full"
-                  >
-                    <CardBody className="flex flex-col items-center gap-4 p-6 bg-[#9799E7] rounded-lg shadow-lg w-full h-full sm:items-start">
-                      <div className="w-24 h-24 rounded-full bg-[#5759BB] flex items-center justify-center">
-                        {card.icon}
-                      </div>
-                      <div className="flex flex-col items-center gap-2 sm:items-start">
-                        <h2 className="text-lg font-bold text-white text-center sm:text-left min-h-[3rem] md:min-h-0">
-                          {card.title}
-                        </h2>
-                        <p className="text-sm text-white text-center sm:text-left">
-                          {card.description}
-                        </p>
-                      </div>
-                    </CardBody>
-                  </Card>
-                </motion.div>
+                  <CardBody className="flex flex-col items-center gap-4 p-6 bg-[#9799E7] rounded-lg shadow-lg w-full h-full sm:items-start">
+                    <div className="w-24 h-24 rounded-full bg-[#5759BB] flex items-center justify-center">
+                      {card.icon}
+                    </div>
+                    <div className="flex flex-col items-center gap-2 sm:items-start">
+                      <h2 className="text-lg font-bold text-white text-center sm:text-left min-h-[3rem] md:min-h-0">
+                        {card.title}
+                      </h2>
+                      <p className="text-sm text-white text-center sm:text-left">
+                        {card.description}
+                      </p>
+                    </div>
+                  </CardBody>
+                </Card>
               </a>
             ))}
           </div>
