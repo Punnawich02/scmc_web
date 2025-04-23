7ad6b87b045df7415cf142d67edd8a79086a305c
Punnawich02 <punnawich_dachin@cmu.ac.th>
2025-04-23 10:43:38 +0700

    fix wrong path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,31 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
- 
-export default createMiddleware(routing);
- 
+// middleware.ts
+import { NextRequest } from "next/server";
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+
+const intlMiddleware = createMiddleware(routing);
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const firstSegment = pathname.split("/")[1];
+  function isValidLocale(locale: string): locale is (typeof routing)['locales'][number] {
+    return (routing.locales as readonly string[]).includes(locale);
+  }
+  
+  
+  // ถ้า first segment ไม่ใช่ locale ที่รองรับ
+  if (firstSegment && !isValidLocale(firstSegment)) {
+    const newPathname = pathname.replace(/^\/[^\/]+/, ''); // ลบ /de
+    const url = request.nextUrl.clone();
+    url.pathname = `/${routing.defaultLocale}${newPathname}`;
+    return Response.redirect(url);
+  }
+  
+
+  // หาก locale ถูกต้อง → ใช้ next-intl middleware ตามปกติ
+  return intlMiddleware(request);
+}
+
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
